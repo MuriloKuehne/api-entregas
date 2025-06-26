128e1531ebd73d6529545b63b83c0f32c200ca65
Murilo <murkuehne@gmail.com>
2025-06-26 20:38:53 -0300

    Criando Controller e Rota de Logs da Entrega

---
+++ src/controllers/delivery-logs-controller.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express"
+import { prisma } from "@/database/prisma"
+import z from "zod"
+
+class DeliveryLogsController {
+  async create(request: Request, response: Response) {
+    return response.json({ message: "ok" })
+  }
+}
+export { DeliveryLogsController }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express"
 import { usersRoutes } from "./users-routes"
 import { sessionsRoutes } from "./sessions-route"
 import { deliveriesRoutes } from "./deliveries-route"
+import { deliveryLogsRoutes } from "./delivery-logs-routes"
 
 const routes = Router()
 
 routes.use("/users", usersRoutes)
 routes.use("/sessions", sessionsRoutes)
 routes.use("/deliveries", deliveriesRoutes)
+routes.use("/delivery-logs", deliveryLogsRoutes)
 
 export { routes }
