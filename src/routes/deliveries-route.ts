1bf6102e80ec4704292cf0288ca5ee60406998e7
Murilo <murkuehne@gmail.com>
2025-06-26 11:17:20 -0300

    Verificando Autorização

--- src/routes/deliveries-route.ts
+++ src/routes/deliveries-route.ts
@@ -1,11 +1,12 @@
 import { Router } from "express"
 import { ensureAuthenticated } from "@/middlewares/ensure-authenticated"
 import { DeliveriesController } from "@/controllers/deliveries-controller"
+import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization"
 
 const deliveriesRoutes = Router()
 const deliveriesController = new DeliveriesController()
 
-deliveriesRoutes.use(ensureAuthenticated)
+deliveriesRoutes.use(ensureAuthenticated, verifyUserAuthorization(["sale"]))
 deliveriesRoutes.post("/", ensureAuthenticated, deliveriesController.create)
 
 export { deliveriesRoutes }
